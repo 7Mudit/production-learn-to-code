082ca5dfeb3d72b9534797890c6dd475d73d4fe8
Mudit Kapoor <muditk441@gmail.com>
2024-02-04 04:24:52 +0530

    updated loaders

--- app/nptel-notes/loading.tsx
+++ app/nptel-notes/loading.tsx
@@ -1,12 +1,6 @@
-import Link from "next/link";
-
-import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton";
-import Image from "next/image";
-
 const Loading = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center ">
+    <div className="w-screen bg-white h-screen flex items-center justify-center ">
       <video autoPlay muted loop>
         <source src="/loading-splash.mp4" type="video/mp4" />
       </video>
--- app/privacy-policy/loading.tsx
+++ app/privacy-policy/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center ">
+    <div className="w-screen bg-white h-screen flex items-center justify-center ">
       <video autoPlay muted loop>
         <source src="/loading-splash.mp4" type="video/mp4" />
       </video>
